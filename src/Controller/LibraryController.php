a0b9ad1794d1672e080ca0de1a8968326638b3ae
Binay <binayprogramming@gmail.com>
2020-11-14 18:50:06 +0100

    Services and containers

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,21 +2,35 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request};
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
-
     /**
      * @Route("library/list",name="library_list")
      */
 
-    public function list()
+    public function list(Request $request, LoggerInterface $logger) //We get the request params with this
     {
-        $response = new Response();
-        $response->setContent('<div>Hola Mundo</div>');
+        $title = $request->get('title'); // Getting out title params off from library/list?title="A book's name"
+        $logger->info('List action called');
+        $response = new JsonResponse();
+        $response->setData([
+            'success' => true,
+            'data' => [[
+                'id' => 1,
+                'title' => 'Un libro muy buenooo'
+            ], [
+                'id' => 2,
+                'title' => 'Un libro muy buenissimo'
+            ], [
+                'id' => 3,
+                'title' => $title
+            ]]
+        ]);
         return $response;
     }
 }
